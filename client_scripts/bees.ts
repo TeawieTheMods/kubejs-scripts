360f3f9ed9b98cd0e74f475082b48afc6a77306e
Skye <me@skye.vg>
2023-11-12 21:48:55 +0900

    feat: easy bees

---
+++ client_scripts/bees.ts
@@ -0,0 +1,8 @@
+import bees from "../common/bees"
+
+ClientEvents.lang("en_us", event => {
+    for (const id in bees) {
+        console.log("adding lang:", `entity.productivebees.${id}_bee`, bees[id].name)
+        event.add(`entity.productivebees.${id}_bee`, bees[id].name)
+    }
+})
--- server_scripts/bees.ts
+++ server_scripts/bees.ts
@@ -1,161 +1,172 @@
+import bees from "../common/bees"
+
 ServerEvents.highPriorityData(event => {
-	let json = {
-		"primaryColor": "#bddada",
-		"secondaryColor": "#d0581f",
-		"flowerTag": "minecraft:flowers",
-		"name": "Baconator",
-		"conditions": [
-			{
-				"type": "forge:mod_loaded",
-				"modid": "productivebees"
-			}
-		]
-	}
-	event.addJson(`kubejs:productivebees/productivebees/bacon.json`, json as any)
+    for (const id in bees) {
+        event.addJson(`kubejs:productivebees/productivebees/${id}.json`, bees[id].bee as any)
+    }
 })
 
 ServerEvents.recipes(event => {
-	event.custom({
-		type: "productivebees:bee_breeding",
-		parent1: "productivebees:zombie",
-		parent2: "productivebees:farmer_bee",
-		offspring: [
-			"kubejs:bacon"
-		],
-		conditions: [
-			{
-				type: "productivebees:bee_exists",
-				bee: "productivebees:zombie"
-			},
-			{
-				type: "productivebees:bee_exists",
-				bee: "productivebees:bacon"
-			}
-		]
-	} as any)
-})
+    for (const id in bees) {
+        const bee_id = `kubejs:${id}`;
+        const bee = bees[id];
+        if (bee.breeding_recipe != undefined) {
+            event.custom({
+                type: "productivebees:bee_breeding",
+                parent1: bee.breeding_recipe[0],
+                parent2: bee.breeding_recipe[1],
+                offspring: [
+                    bee_id
+                ],
+                conditions: [
+                    {
+                        type: "productivebees:bee_exists",
+                        bee: bee.breeding_recipe[0]
+                    },
+                    {
+                        type: "productivebees:bee_exists",
+                        bee: bee.breeding_recipe[1]
+                    },
+                    {
+                        type: "productivebees:bee_exists",
+                        bee: bee_id
+                    }
+                ]
+            } as any)
+        }
 
-ServerEvents.recipes(event => {
-	event.custom({
-		"type": "productivebees:bee_conversion",
-		"source": "productivebees:zombie",
-		"result": "kubejs:bacon",
-		"item": {
-			"item": "minecraft:porkchop"
-		},
-		"conditions": [
-			{
-				"type": "productivebees:bee_exists",
-				"bee": "productivebees:zombie"
-			},
-			{
-				"type": "productivebees:bee_exists",
-				"bee": "kubejs:bacon"
-			}
-		]
-	} as any)
-})
+        if (bee.conversion_recipe != undefined) {
+            event.custom({
+                "type": "productivebees:bee_conversion",
+                "source": bee.conversion_recipe.from,
+                "result": bee_id,
+                "item": {
+                    "item": bee.conversion_recipe.with
+                },
+                "conditions": [
+                    {
+                        "type": "productivebees:bee_exists",
+                        "bee": bee.conversion_recipe.from
+                    },
+                    {
+                        "type": "productivebees:bee_exists",
+                        "bee": bee_id
+                    }
+                ]
+            } as any)
+        }
 
-ServerEvents.recipes(event => {
-	event.custom(
-		{
-			"type": "productivebees:advanced_beehive",
-			"ingredient": "kubejs:bacon",
-			"results": [
-				{
-					"item": {
-						"type": "forge:nbt",
-						"item": "productivebees:configurable_honeycomb",
-						"nbt": {
-							"EntityTag": {
-								"type": "kubejs:bacon"
-							}
-						}
-					}
-				}
-			],
-			"conditions": [
-				{
-					"type": "productivebees:bee_exists",
-					"bee": "kubejs:bacon"
-				}
-			]
-		}  as any
-	)
-})
+        event.custom(
+            {
+                "type": "productivebees:advanced_beehive",
+                "ingredient": bee_id,
+                "results": [
+                    {
+                        "item": {
+                            "type": "forge:nbt",
+                            "item": "productivebees:configurable_honeycomb",
+                            "nbt": {
+                                "EntityTag": {
+                                    "type": bee_id
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "item": {
+                            "item": "the_bumblezone:pollen_puff"
+                        },
+                        "chance": 5
+                    }
+                ],
+                "conditions": [
+                    {
+                        "type": "productivebees:bee_exists",
+                        "bee": bee_id
+                    }
+                ]
+            }  as any
+        )
 
-ServerEvents.recipes(event => {
-	event.custom(
-		{
-			"type": "productivebees:centrifuge",
-			"ingredient": {
-				"type": "forge:nbt",
-				"item": "productivebees:configurable_honeycomb",
-				"nbt": {
-					"EntityTag": {
-						"type": "kubejs:bacon"
-					}
-				}
-			},
-			"outputs": [
-				{
-					"item": {
-						"item": "minecraft:porkchop"
-					},
-					"min": 1,
-					"max": 4,
-					"chance": 90
-				},
-				{
-					"item": {
-						"tag": "forge:wax"
-					}
-				},
-				{
-					"fluid": {
-							"fluid": "productivebees:honey"
-					},
-					"amount": 50
-				}
-			]
-		} as any
-	)
-})
+        const comb_output: any = {
+            "item": {
+                "item": bee.comb_output.item
+            }
+        };
 
-ServerEvents.recipes(event => {
-	event.custom(
-		{
-			"type": "productivebees:centrifuge",
-			"ingredient": {
-				"type": "forge:nbt",
-				"item": "productivebees:configurable_comb",
-				"nbt": {
-					"EntityTag": {
-						"type": "kubejs:bacon"
-					}
-				}
-			},
-			"outputs": [
-				{
-					"item": {
-						"item": "minecraft:porkchop"
-					},
-					"min": 4,
-					"max": 16,
-					"chance": 90
-				},
-				{
-					"item": {
-						"tag": "forge:wax"
-					}
-				},
-				{
-					"fluid": {
-							"fluid": "productivebees:honey"
-					},
-					"amount": 200
-				}
-			]
-		} as any
-	)
+        if (bee.comb_output.range != undefined) {
+            comb_output.min = bee.comb_output.range.min
+            comb_output.max = bee.comb_output.range.max
+        }
+
+        if (bee.comb_output.chance != undefined) {
+            comb_output.chance = bee.comb_output.chance
+        }
+
+        event.custom(
+            {
+                "type": "productivebees:centrifuge",
+                "ingredient": {
+                    "type": "forge:nbt",
+                    "item": "productivebees:configurable_honeycomb",
+                    "nbt": {
+                        "EntityTag": {
+                            "type": bee_id
+                        }
+                    }
+                },
+                "outputs": [
+                    comb_output,
+                    {
+                        "item": {
+                            "tag": "forge:wax"
+                        }
+                    },
+                    {
+                        "fluid": {
+                                "fluid": "productivebees:honey"
+                        },
+                        "amount": 50
+                    }
+                ]
+            } as any
+        )
+
+        if (bee.comb_output.do_blocks) {
+            if (bee.comb_output.range != undefined) {
+                comb_output.min = bee.comb_output.range.min * 4
+                comb_output.max = bee.comb_output.range.max * 4
+            }
+            event.custom(
+                {
+                    "type": "productivebees:centrifuge",
+                    "ingredient": {
+                        "type": "forge:nbt",
+                        "item": "productivebees:configurable_comb",
+                        "nbt": {
+                            "EntityTag": {
+                                "type": bee_id
+                            }
+                        }
+                    },
+                    "outputs": [
+                        comb_output,
+                        {
+                            "item": {
+                                "tag": "forge:wax"
+                            },
+                            "min": 4,
+                            "max": 4
+                        },
+                        {
+                            "fluid": {
+                                    "fluid": "productivebees:honey"
+                            },
+                            "amount": 200
+                        }
+                    ]
+                } as any
+            )
+        }
+    }
 })
