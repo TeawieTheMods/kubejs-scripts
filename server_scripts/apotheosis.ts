0051dc468bc9e3df24046d41374950cb8a0fec72
Fnige <35159947+FnigePython@users.noreply.github.com>
2023-11-14 22:26:54 +1100

    Stuff and things

--- server_scripts/apotheosis.ts
+++ server_scripts/apotheosis.ts
@@ -1,8 +1,17 @@
 let highRate = 2;
-
-
 let lowRate = highRate * 1.5
 
+/* Template
+ServerEvents.highPriorityData(event => {
+	let json = {
+		
+	};
+
+	event.addJson(`apotheosis:path/to/thingy.json`, json as any)
+}) */
+
+
+// shallow rogue spawner
 ServerEvents.highPriorityData(event => {
 	let json = {
 			"forge:conditions": [{
@@ -39,7 +48,7 @@ ServerEvents.highPriorityData(event => {
 	event.addJson(`apotheosis:worldgen/placed_feature/rogue_spawner.json`, json as any)
 })
 
-// deep
+// deep rogue spawner
 ServerEvents.highPriorityData(event => {
 	let json = {
 		"forge:conditions": [{
@@ -74,4 +83,159 @@ ServerEvents.highPriorityData(event => {
 
 	console.log("Thats deep");
 	event.addJson(`apotheosis:worldgen/placed_feature/rogue_spawner_deep.json`, json as any)
+})
+
+// elytra affix
+ServerEvents.highPriorityData(event => {
+	let json = {
+		"conditions": [
+			{ "type": "forge:false" }
+		]
+	};
+
+	event.addJson(`apotheosis:affixes/armor/attribute/winged.json`, json as any)
+})
+
+// high boss dungeon
+ServerEvents.highPriorityData(event => {
+	let json = {
+		"forge:conditions": [{
+			"type": "apotheosis:module",
+			"module": "adventure"
+		}],
+		"feature": "apotheosis:boss_dungeon",
+		"placement": [{
+				"type": "minecraft:count",
+				"count": highRate
+			},
+			{
+				"type": "minecraft:in_square"
+			},
+			{
+				"type": "minecraft:height_range",
+				"height": {
+					"type": "minecraft:uniform",
+					"max_inclusive": {
+						"below_top": 0
+					},
+					"min_inclusive": {
+						"absolute": 0
+					}
+				}
+			},
+			{
+				"type": "minecraft:biome"
+			}
+		]
+	};
+
+	event.addJson(`apotheosis:worldgen/placed_feature/boss_dungeon.json`, json as any)
+})
+
+// high boss dungeon 2
+ServerEvents.highPriorityData(event => {
+	let json = {
+		"forge:conditions": [{
+			"type": "apotheosis:module",
+			"module": "adventure"
+		}],
+		"feature": "apotheosis:boss_dungeon_2",
+		"placement": [{
+				"type": "minecraft:count",
+				"count": highRate
+			},
+			{
+				"type": "minecraft:in_square"
+			},
+			{
+				"type": "minecraft:height_range",
+				"height": {
+					"type": "minecraft:uniform",
+					"max_inclusive": {
+						"below_top": 0
+					},
+					"min_inclusive": {
+						"absolute": 0
+					}
+				}
+			},
+			{
+				"type": "minecraft:biome"
+			}
+		]
+	};
+
+	event.addJson(`apotheosis:worldgen/placed_feature/boss_dungeon_2.json`, json as any)
+})
+
+// deep boss dungeon
+ServerEvents.highPriorityData(event => {
+	let json = {
+		"forge:conditions": [{
+			"type": "apotheosis:module",
+			"module": "adventure"
+		}],
+		"feature": "apotheosis:boss_dungeon",
+		"placement": [{
+				"type": "minecraft:count",
+				"count": lowRate
+			},
+			{
+				"type": "minecraft:in_square"
+			},
+			{
+				"type": "minecraft:height_range",
+				"height": {
+					"type": "minecraft:uniform",
+					"max_inclusive": {
+						"absolute": -1
+					},
+					"min_inclusive": {
+						"above_bottom": 6
+					}
+				}
+			},
+			{
+				"type": "minecraft:biome"
+			}
+		]
+	};
+
+	event.addJson(`apotheosis:worldgen/placed_feature/boss_dungeon_deep.json`, json as any)
+})
+
+// deep boss dungeon 2
+ServerEvents.highPriorityData(event => {
+	let json = {
+		"forge:conditions": [{
+			"type": "apotheosis:module",
+			"module": "adventure"
+		}],
+		"feature": "apotheosis:boss_dungeon_2",
+		"placement": [{
+				"type": "minecraft:count",
+				"count": lowRate
+			},
+			{
+				"type": "minecraft:in_square"
+			},
+			{
+				"type": "minecraft:height_range",
+				"height": {
+					"type": "minecraft:uniform",
+					"max_inclusive": {
+						"absolute": -1
+					},
+					"min_inclusive": {
+						"above_bottom": 6
+					}
+				}
+			},
+			{
+				"type": "minecraft:biome"
+			}
+		]
+	};
+
+	event.addJson(`apotheosis:worldgen/placed_feature/boss_dungeon_2_deep.json`, json as any)
 })
---
+++ server_scripts/recipe_removal.ts
@@ -0,0 +1,7 @@
+let outputRemove = ["botania:mana_fluxfield"]
+
+ServerEvents.recipes(event => {
+	for (let i in outputRemove) {
+		event.remove({output: `${outputRemove[i]}` as any})
+	}
+})
