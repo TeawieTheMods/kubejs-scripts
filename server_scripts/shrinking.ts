ef58a640cfae3d894059c948bc57f00d17250fed
Fnige <35159947+FnigePython@users.noreply.github.com>
2023-11-24 20:47:09 +1100

    shrinking

---
+++ server_scripts/shrinking.ts
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+	(event.shaped as any)(`1x teawieks:scaler`,
+	["iei", "igi", "ibi"],
+	{
+		b: "minecraft:stone_button",
+		e: "minecraft:ender_pearl",
+		g: "minecraft:glass",
+		i: "minecraft:iron_ingot"
+	})
+})
